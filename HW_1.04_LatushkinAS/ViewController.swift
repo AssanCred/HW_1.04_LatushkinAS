8694d8b654a9086e85b0859b8ebc5b9ac7b5a216
AssanCred <temix@live.ru>
2023-02-07 11:09:04 +0700

    firstRealise

--- HW_1.04_LatushkinAS/ViewController.swift
+++ HW_1.04_LatushkinAS/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func actionRedSlider() {
+        redLabel.text = String(format:"%.2f", redSlider.value)
+        updateColor()
+    }
+   
+    @IBAction func actionGreenSlider() {
+        greenLabel.text = String(format:"%.2f", greenSlider.value)
+        updateColor()
+    }
+    
+    @IBAction func actionBlueSlider() {
+        blueLabel.text = String(format:"%.2f", blueSlider.value)
+        updateColor()
+    }
+    
+    private func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSlider.value > 0 {
+            red = CGFloat(redSlider.value)
+        }
+        if greenSlider.value > 0 {
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSlider.value > 0 {
+            blue = CGFloat(blueSlider.value)
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
 
 }
 
